19615c6b8952a45ad2497252f99dbaeacb8525ab
NawwafNaufal <nawwafnaufal03@gmail.com>
2023-12-15 20:59:10 +0700

    New

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -26,46 +26,30 @@
         <div class="sidebar-heading">
             <?= $m['menu']; ?>
         </div>
+
+
+        <?php
+        $menuId = $m['id'];
+        $querySubMenu = "SELECT *
+                    FROM `user_sub_menu` JOIN `user_menu`
+                    ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+                    WHERE `user_sub_menu`.`menu_id` = $menuId
+                    AND `user_sub_menu`.`is_active` =1;
+";
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
+        <?php foreach ($subMenu as $sm) : ?>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                    <i class="<?= $sm['icon']; ?>"></i>
+                    <span><?= $sm['title']; ?></span></a>
+            </li>
+        <?php endforeach; ?>
+        <hr class="sidebar-divider">
     <?php endforeach; ?>
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Member
-    </div>
-
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-solid fa-star"></i>
-            <span>Membership</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
-
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>My Profile</span></a>
-    </li>
-
 
     <!-- Divider -->
-    <hr class="sidebar-divider">
 
     <!-- Heading -->
 
