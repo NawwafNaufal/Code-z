a09987243013cf1bf5ba0a029989c1c9028163bb
NawwafNaufal <nawwafnaufal03@gmail.com>
2023-12-20 23:47:54 +0700

    Edit Profile

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -20,4 +20,22 @@ class User extends CI_Controller
         $this->load->view('user/index', $data);
         $this->load->view('templates/footer',);
     }
+
+    public function edit()
+    {
+        $data['title'] = 'Edit Profile';
+        $data['user'] = $this->db->get_where('user', ['email' =>
+        $this->session->userdata('email')])->row_array();
+
+        
+
+        if ($this->form_validation->run() == false) {
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('user/edit', $data);
+            $this->load->view('templates/footer',);
+        }
+    }
 }
