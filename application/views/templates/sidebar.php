10b1fe21e9777f7ab01e6003a4e5668b49ac6530
NawwafNaufal <nawwafnaufal03@gmail.com>
2023-12-23 22:22:15 +0700

    Beranda

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,5 +1,10 @@
 <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<style>
+    #accordionSidebar {
+        background-color: #08A1CD;
+    }
+</style>
+<ul class="navbar-nav  sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
